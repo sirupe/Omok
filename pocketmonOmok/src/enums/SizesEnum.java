b21fc6769d32a15e144564337cb9fd47dc478a93
Kimtaesung <cjsguszhf@gmail.com>
2016-09-10 19:06:41 +0900

    Kim LoginFrame add

--- pocketmonOmok/src/enums/SizesEnum.java
+++ pocketmonOmok/src/enums/SizesEnum.java
@@ -1,6 +1,32 @@
 package enums;
 
+
 public enum SizesEnum {
-	SIZE_X,
-	SIZE_Y;
+	SIZE_X(1300),
+	SIZE_Y(900),
+	SIZE_ICON_X(100),
+	SIZE_ICON_Y(100),
+	SIZE_LABEL_X(100),
+	SIZE_LABEL_Y(50),
+	SIZE_TEXT_X(150),
+	SIZE_TEXT_Y(30),
+	SIZE_JOIN_ICON_Y(50),
+	SIZE_PW_ICON_X(110),
+	SCREEN_SIZE();
+	
+	private int size;
+	
+	private SizesEnum() {}
+	
+	private SizesEnum(int x) {
+		this.size = x;
+	}
+	
+	public int getSizeX() {
+		return size;
+	}
+	
+	public int getSizeY() {
+		return size;
+	}
 }
