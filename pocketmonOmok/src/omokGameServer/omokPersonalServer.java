ba084a1fc1e59c073c3b3bbd750fb611b018103d
EUNJUNG\sirupe!!! <valensia_@naver.com>
2016-09-10 14:04:17 +0900

    eunjung server update

--- pocketmonOmok/src/omokGameServer/omokPersonalServer.java
+++ pocketmonOmok/src/omokGameServer/omokPersonalServer.java
@@ -4,26 +4,54 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+import datas.UserPositionIndex;
+
 public class OmokPersonalServer extends Thread{
 	private OmokServer omokServer;
 	private ObjectInputStream serverInputStream;
 	private ObjectOutputStream serverOutputStream;
-	private String test; 
 	
-	private String test1;
 	
-//	
-//	public omokPersonalServer(OmokServer omokServer) throws IOException {
-//		this.omokServer = omokServer;
-//		this.serverInputStream = new ObjectInputStream(this.omokServer.getSocket().getInputStream());
-//		this.serverOutputStream = new ObjectOutputStream(this.omokServer.getSocket().getOutputStream());
-//	}
+	
+	public OmokPersonalServer(OmokServer omokServer) throws IOException {
+		this.omokServer = omokServer;
+		this.serverInputStream  = new ObjectInputStream(this.omokServer.getSocket().getInputStream());
+		this.serverOutputStream = new ObjectOutputStream(this.omokServer.getSocket().getOutputStream());
+	}
 	
 	@Override
 	public void run() {
+		boolean isAccept = true;
 		//TODO ¿©±â¼­ 
-		while(true) {
-			
+		try {
+			while(isAccept) {
+				Object object = this.serverInputStream.readObject();
+				UserPositionIndex userPosition = (UserPositionIndex)object;
+				switch(userPosition.getPosition()) {
+				case POSITION_LOGIN :             
+					break;
+				case POSITION_WAITING_ROON :      
+					break;
+				case POSITION_JOIN :              
+					break;
+				case POSITION_FIND_ID :           
+					break;
+				case POSITION_FIND_PW :           
+					break;
+				case POSITION_GAME_ROOM :         
+					break;
+				case POSITION_STORE :             
+					break;
+				case POSITION_OTHER_USER_INFO :   
+					break;
+				case POSITION_MODIFY_MY_INFO :    
+					break;
+				case POSITION_EXIT :              
+					break;
+				}
+			}
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
