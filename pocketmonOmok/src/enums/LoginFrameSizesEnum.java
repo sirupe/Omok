7f80e56b4aec0a63c43940ed99d28ff3929cdcb5
JungSujin <tnwls147258@naver.com>
2016-09-13 22:34:38 +0900

    sujin joinFrameUpdate

--- pocketmonOmok/src/enums/LoginFrameSizesEnum.java
+++ pocketmonOmok/src/enums/LoginFrameSizesEnum.java
@@ -5,73 +5,7 @@ import java.awt.Toolkit;
 // 태성(경로)
 
 public enum LoginFrameSizesEnum {
-<<<<<<< HEAD
-   SCREEN_SIZE(Toolkit.getDefaultToolkit().getScreenSize()),
-   LOGIN_FRAME_SIZE_WIDTH((int)(LoginFrameSizesEnum.SCREEN_SIZE.getDimension().getWidth() * 0.7)),
-   LOGIN_FRAME_SIZE_HEIGHT((int)(LOGIN_FRAME_SIZE_WIDTH.getSize() * 0.7)),
-   LOGIN_FRAME_POSITION_X((int)((SCREEN_SIZE.getDimension().getWidth() / 2) - (LoginFrameSizesEnum.LOGIN_FRAME_SIZE_WIDTH.getSize() / 2))),
-   LOGIN_FRAME_POSITION_Y((int)((SCREEN_SIZE.getDimension().getHeight() / 2)) - (LoginFrameSizesEnum.LOGIN_FRAME_SIZE_HEIGHT.getSize() / 2)),   
-   
-   LOGIN_RESOURCE_ID_POSITION_X((int)(LoginFrameSizesEnum.LOGIN_FRAME_SIZE_WIDTH.getSize() * 0.36)),
-   LOGIN_RESOURCE_ID_POSITION_Y((int)(LoginFrameSizesEnum.LOGIN_FRAME_SIZE_HEIGHT.getSize() * 0.34)),
-      
-   LOGIN_RESOURCE_PASSWORD_POSITION_X(LoginFrameSizesEnum.LOGIN_RESOURCE_ID_POSITION_X.getSize()),
-   LOGIN_RESOURCE_PASSWORD_POSITION_Y(LoginFrameSizesEnum.LOGIN_RESOURCE_ID_POSITION_Y.getSize() + 58),
-   
-   LOGIN_RESOURCE_LOGIN_BUTTON_POSITION_X(LoginFrameSizesEnum.LOGIN_RESOURCE_ID_POSITION_X.getSize() + 130),
-   LOGIN_RESOURCE_LOGIN_BUTTON_POSITION_Y(LoginFrameSizesEnum.LOGIN_RESOURCE_ID_POSITION_Y.getSize() + 178),
-   
-   LOGIN_RESOURCE_JOIN_BUTTON_POSITION_X(LoginFrameSizesEnum.LOGIN_RESOURCE_ID_POSITION_X.getSize()),
-   LOGIN_RESOURCE_JOIN_BUTTON_POSITION_Y(LoginFrameSizesEnum.LOGIN_RESOURCE_ID_POSITION_Y.getSize() + 126),
-   
-   LOGIN_RESOURCE_SEARCHID_BUTTON_POSITION_X(LoginFrameSizesEnum.LOGIN_RESOURCE_ID_POSITION_X.getSize() + 130),
-   LOGIN_RESOURCE_SEARCHID_BUTTON_POSITION_Y(LoginFrameSizesEnum.LOGIN_RESOURCE_ID_POSITION_Y.getSize() + 126),
-   
-   LOGIN_RESOURCE_SEARCHPW_BUTTON_POSITION_X(LoginFrameSizesEnum.LOGIN_RESOURCE_ID_POSITION_X.getSize() + 250),
-   LOGIN_RESOURCE_SEARCHPW_BUTTON_POSITION_Y(LoginFrameSizesEnum.LOGIN_RESOURCE_ID_POSITION_Y.getSize() + 126),
-   
-   LOGIN_RESOURCE_ID_FIELD_POSITION_X(LoginFrameSizesEnum.LOGIN_RESOURCE_ID_POSITION_X.getSize() + 102),
-   LOGIN_RESOURCE_ID_FIELD_POSITION_Y(LoginFrameSizesEnum.LOGIN_RESOURCE_ID_POSITION_Y.getSize() + 10),
-   
-   LOGIN_RESOURCE_PW_FIELD_POSITION_X(LoginFrameSizesEnum.LOGIN_RESOURCE_ID_POSITION_X.getSize() + 102),
-   LOGIN_RESOURCE_PW_FIELD_POSITION_Y(LoginFrameSizesEnum.LOGIN_RESOURCE_ID_POSITION_Y.getSize() + 68),
-   
-   LOGIN_ICON_WIDTH((int)(LoginFrameSizesEnum.LOGIN_FRAME_SIZE_WIDTH.getSize() * 0.08)),
-   LOGIN_ICON_HEIGHT((int)(LoginFrameSizesEnum.LOGIN_FRAME_SIZE_HEIGHT.getSize() * 0.11)),
-   LOGIN_ICON_POSITION_X((int)((LoginFrameSizesEnum.LOGIN_FRAME_SIZE_WIDTH.getSize() / 2) - (LoginFrameSizesEnum.LOGIN_ICON_WIDTH.getSize() / 2))),
-   
-   SIZE_LABEL_WIDTH((int)(LoginFrameSizesEnum.LOGIN_FRAME_SIZE_WIDTH.getSize() * 0.08)),
-   SIZE_LABEL_HEIGHT((int)(LoginFrameSizesEnum.LOGIN_FRAME_SIZE_HEIGHT.getSize() * 0.05)),
-   
-   SIZE_TEXT_WIDTH((int)(LoginFrameSizesEnum.LOGIN_FRAME_SIZE_WIDTH.getSize() * 0.14)),
-   SIZE_TEXT_HEIGHT((int)(LoginFrameSizesEnum.LOGIN_FRAME_SIZE_HEIGHT.getSize() * 0.03)),
-   
-   SIZE_JOIN_ICON_HEIGTH((int)(LoginFrameSizesEnum.LOGIN_FRAME_SIZE_WIDTH.getSize() * 0.03)),
-   SIZE_PW_ICON_WIDTH((int)(LoginFrameSizesEnum.LOGIN_FRAME_SIZE_HEIGHT.getSize() * 0.12)),
-   
-   //============================================================================================
-   
-   LOGIN_ACCESS_SIZE_WIDTH((int)(LoginFrameSizesEnum.SCREEN_SIZE.getDimension().getWidth() * 0.2)),
-   LOGIN_ACCESS_SIZE_HEIGHT((int)(LoginFrameSizesEnum.SCREEN_SIZE.getDimension().getHeight() * 0.27)),
-   
-   LOGIN_ACCESS_FRAME_POSITION_X((int)((SCREEN_SIZE.getDimension().getWidth() / 2) - (LoginFrameSizesEnum.LOGIN_ACCESS_SIZE_WIDTH.getSize() / 2))),
-   LOGIN_ACCESS_FRAME_POSITION_Y((int)((SCREEN_SIZE.getDimension().getHeight() / 2) - (LoginFrameSizesEnum.LOGIN_ACCESS_SIZE_HEIGHT.getSize() / 2))),
-   
-   LOGIN_ACCESS_LABEL_WIDTH(300),
-   LOGIN_ACCESS_LABEL_HEIGTH(100),
-   
-   LOGIN_ACCESS_LABEL_POSITION_X((int)(LoginFrameSizesEnum.LOGIN_ACCESS_SIZE_WIDTH.getSize() / 4.2)),
-   LOGIN_ACCESS_LABEL_POSITION_Y((int)(LoginFrameSizesEnum.LOGIN_ACCESS_SIZE_HEIGHT.getSize() / 6)),
-   
-   LOGIN_ACCESS_OK_BUTTON_WIDTH(100),
-   LOGIN_ACCESS_OK_BUTTON_HEIGHT(50),
-   
-   LOGIN_ACCESS_OK_BUTTON_POSITION_X((int)(LoginFrameSizesEnum.LOGIN_ACCESS_SIZE_WIDTH.getSize() / 2.66)),
-   LOGIN_ACCESS_OK_BUTTON_POSITION_Y((int)(LoginFrameSizesEnum.LOGIN_ACCESS_SIZE_HEIGHT.getSize() / 2));
-   
-   private int size;
-=======
-	//전체화면 사이즈
+
 	SCREEN_SIZE(Toolkit.getDefaultToolkit().getScreenSize()),
 	
 	//로그인 프레임 가로 길이
@@ -174,7 +108,6 @@ public enum LoginFrameSizesEnum {
 	LOGIN_ACCESS_OK_BUTTON_POSITION_Y((int)(LOGIN_ACCESS_SIZE_HEIGHT.getSize() / 2));
 	
 	private int size;
->>>>>>> 67fedcf69f32850bbdca2897bc10230834660c17
 
    private Dimension dimension;
    
