4864764b559fee339083d462a8c9b9f4d0a55759
JungSujin <tnwls147258@naver.com>
2016-09-24 01:19:57 +0900

    sujin update

--- pocketmonOmok/src/enums/frames/GamePayRoomEnum.java
+++ pocketmonOmok/src/enums/frames/GamePayRoomEnum.java
@@ -7,23 +7,99 @@ import java.awt.Rectangle;
 import java.awt.Toolkit;
 
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
+import javax.swing.plaf.BorderUIResource.MatteBorderUIResource;
 
 public enum GamePayRoomEnum {
 Screen_SIZE(Toolkit.getDefaultToolkit().getScreenSize()),
 	
-	GAME_PAY_ROOM_WIDTH((int)(Screen_SIZE.getDimension().getWidth() * 0.25)),
-	GAME_PAY_ROOM_HEIGHT((int)(GAME_PAY_ROOM_WIDTH.getSize() * 1.2)),	
+	GAME_PAY_ROOM_WIDTH((int)(Screen_SIZE.getDimension().getWidth() * 0.22)),
+	GAME_PAY_ROOM_HEIGHT((int)(GAME_PAY_ROOM_WIDTH.getSize() * 1)),	
 	GAME_PAY_ROOM_POSITION_X((int)((Screen_SIZE.getDimension().getWidth() / 2) - (GAME_PAY_ROOM_WIDTH.getSize() / 2 ))),
 	GAME_PAY_ROOM_POSITION_Y((int)((Screen_SIZE.getDimension().getHeight() / 2) - (GAME_PAY_ROOM_HEIGHT.getSize() / 2))),
 	//===========================================================================
+	//아이템 크기
+	GAME_ROOM_PAY_ITEM_LABEL(new Rectangle(
+			(int)(GAME_PAY_ROOM_POSITION_X.getSize() / 100 * 18),
+			(int)(GAME_PAY_ROOM_POSITION_Y.getSize() / 100 * 10),
+			(int)(GAME_PAY_ROOM_WIDTH.getSize() / 100 * 35),
+			(int)(GAME_PAY_ROOM_HEIGHT.getSize() / 100 * 35)
+			)),
+	
+	//기본 1000원
+	GAME_ROOM_PAY_BASICMONEY_LABEL(new Rectangle(
+			(int)(GAME_PAY_ROOM_POSITION_X.getSize() / 100 * 1),
+			(int)(GAME_PAY_ROOM_POSITION_Y.getSize() / 100 * 70),
+			(int)(GAME_PAY_ROOM_WIDTH.getSize() / 100 * 20),
+			(int)(GAME_PAY_ROOM_HEIGHT.getSize() / 100 * 15)
+			)),
+	// x 
+	GAME_ROOM_PAY_DIV_LABEL(new Rectangle(
+			(int)(GAME_PAY_ROOM_POSITION_X.getSize() / 100 * 14),
+			(int)(GAME_PAY_ROOM_POSITION_Y.getSize() / 100 * 71),
+			(int)(GAME_PAY_ROOM_WIDTH.getSize() / 100 * 20),
+			(int)(GAME_PAY_ROOM_HEIGHT.getSize() / 100 * 13)
+			)),
+
+	//==
+	GAME_ROOM_PAY_EQUAL_LABEL(new Rectangle(
+			(int)(GAME_PAY_ROOM_POSITION_X.getSize() / 100 * 35),
+			(int)(GAME_PAY_ROOM_POSITION_Y.getSize() / 100 * 76),
+			(int)(GAME_PAY_ROOM_WIDTH.getSize() / 100 * 5),
+			(int)(GAME_PAY_ROOM_HEIGHT.getSize() / 100 * 5)
+			)),
+	//총금액 계산
+	GAME_ROOM_PAY_TOTALMONEY_LABEL(new Rectangle(
+			(int)(GAME_PAY_ROOM_POSITION_X.getSize() / 100 * 40),
+			(int)(GAME_PAY_ROOM_POSITION_Y.getSize() / 100 * 73),
+			(int)(GAME_PAY_ROOM_WIDTH.getSize() / 100 * 28),
+			(int)(GAME_PAY_ROOM_HEIGHT.getSize() / 100 * 10)
+			)),
+//==========Panel크기==================================================
+	GAME_ROOM_AMOUNT_PANEL(new Rectangle(
+			(int)(GAME_PAY_ROOM_POSITION_X.getSize() / 100 * 18),
+			(int)(GAME_PAY_ROOM_POSITION_Y.getSize() / 100 * 72),
+			(int)(GAME_PAY_ROOM_WIDTH.getSize() / 100 * 27),
+			(int)(GAME_PAY_ROOM_HEIGHT.getSize() / 100 * 12)
+			)),
+	
+	GAME_ROOM_USER_AMOUNT_PANEL(new Rectangle(
+			(int)(GAME_PAY_ROOM_POSITION_X.getSize() / 100 * 1),
+			(int)(GAME_PAY_ROOM_POSITION_Y.getSize() / 100 * 1),
+			(int)(GAME_PAY_ROOM_WIDTH.getSize() / 100 * 15),
+			(int)(GAME_PAY_ROOM_HEIGHT.getSize() / 100 * 10)
+			)),
+	
+//=====================================================================	
+	// 결제 버튼
+	GAME_ROOM_PAY_BUTTON(new Rectangle(
+			(int)(GAME_PAY_ROOM_POSITION_X.getSize() / 100 * 18),
+			(int)(GAME_PAY_ROOM_POSITION_Y.getSize() / 100 * 105),
+			(int)(GAME_PAY_ROOM_WIDTH.getSize() / 100 * 16),
+			(int)(GAME_PAY_ROOM_HEIGHT.getSize() / 100 * 10)
+			)),
+	
+	
+	
+	//취소 버튼
+	GAME_ROOM_PAY_CANCEL_BUTTON(new Rectangle(
+			(int)(GAME_PAY_ROOM_POSITION_X.getSize() / 100 * 30),
+			(int)(GAME_PAY_ROOM_POSITION_Y.getSize() / 100 * 105),
+			(int)(GAME_PAY_ROOM_WIDTH.getSize() / 100 * 16),
+			(int)(GAME_PAY_ROOM_HEIGHT.getSize() / 100 * 10)
+			)),
+	
+	
 	
 	
 	//일반 폰트
-	LABELFONT_DEFAULT(new Font("a으라차차",Font.BOLD,17)),
+	LABELFONT_DEFAULT(new Font("a으라차차",Font.BOLD,22)),
 	// 일반색깔
 	LABELCOLOR_DEFAULT(Color.gray),
 	//텍스트 테두리 없애기
-	LABEL_DEFAULT(new EmptyBorder(0,0,0,0));
+	LABEL_DEFAULT(new EmptyBorder(0,0,0,0)),
+	
+	LABEL_LINE(new MatteBorder(0,0,0, 0, Color.pink));
 
 	private Dimension dimension;
 	private int size;
@@ -31,6 +107,7 @@ Screen_SIZE(Toolkit.getDefaultToolkit().getScreenSize()),
 	private Color color;
 	private Rectangle rec;
 	private EmptyBorder emptyBorder;
+	private MatteBorder matteBorder;
 	
 	private GamePayRoomEnum(Dimension dimension) {
 		this.dimension = dimension;
@@ -49,6 +126,12 @@ Screen_SIZE(Toolkit.getDefaultToolkit().getScreenSize()),
 	}
 	private GamePayRoomEnum(EmptyBorder emptyBorder) {
 		this.emptyBorder = emptyBorder;
+	}
+	private GamePayRoomEnum(MatteBorder matteBorder) {
+		this.matteBorder = matteBorder;
+	}
+	public MatteBorder getMatterBorder() {
+		return matteBorder;
 	}
 	public EmptyBorder getEmptyBorder() {
 		return emptyBorder;
--- pocketmonOmok/src/frames/GamePayRoomFrame.java
+++ pocketmonOmok/src/frames/GamePayRoomFrame.java
@@ -1,43 +1,75 @@
 package frames;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import enums.frames.GamePayRoomEnum;
+import enums.frames.GameStoreEnum;
 import enums.frames.searchIdEnum;
 
-public class GamePayRoomFrame extends JFrame{
+public class GamePayRoomFrame extends JFrame {
 	private Image icon;
-	private JLabel basicMoneyJLabel;
-	private JLabel itemQuantityLabel;
-	private JLabel equals;
+	private JLabel imageIcon;
+	private JLabel basicMoneyLabel;
+	private JLabel divLabel;
+	private JTextField userAmountText;
+	private JLabel equalsLabel;
 	private JLabel totalAmountLabel;
+	
+	private JPanel q;
 	
 	private JButton payButton;
 	private JButton cancelButton;
-	private BufferedImage backGround;
+	private Image backGround;
 	
 	public GamePayRoomFrame() throws IOException {
+
 		
+		this.q = new JPanel();
+
+		this.imageIcon = new JLabel(""); //안에 구입할 아이템이 들어 가야 한다.
 		
+		this.imageIcon.setOpaque(true);
+		this.imageIcon.setBackground(Color.pink);
 		
+
+		this.basicMoneyLabel  = new JLabel("1000");
+		this.basicMoneyLabel.setOpaque(true);
+		//this.basicMoneyLabel.setBackground(Color.pink);
+		this.divLabel         = new JLabel("X");
+		this.equalsLabel      = new JLabel("=");
+		this.totalAmountLabel = new JLabel();
+		this.totalAmountLabel.setOpaque(true);
+		this.totalAmountLabel.setBackground(Color.BLUE);
+	
 		
-		//배경화면 정해주기
-		backGround = ImageIO.read(new File(""));
-		this.backGround.getScaledInstance(
-				GamePayRoomEnum.GAME_PAY_ROOM_WIDTH.getSize(),
-				GamePayRoomEnum.GAME_PAY_ROOM_HEIGHT.getSize(),
-				Image.SCALE_SMOOTH);
-		this.setContentPane(new JLabel(new ImageIcon(backGround)));
+		this.payButton = new JButton();
+		this.payButton.setBorderPainted(false);
+		this.payButton.setFocusPainted(false);
+		this.payButton.setContentAreaFilled(false);
 		
+		this.cancelButton = new JButton();
+		this.cancelButton.setBorderPainted(false);
+		this.cancelButton.setFocusPainted(false);
+		this.cancelButton.setContentAreaFilled(false);
+//프레임 화면 크기 위치
 		this.setBounds(
 				GamePayRoomEnum.GAME_PAY_ROOM_POSITION_X.getSize(),
 				GamePayRoomEnum.GAME_PAY_ROOM_POSITION_Y.getSize(),
@@ -49,29 +81,85 @@ public class GamePayRoomFrame extends JFrame{
 		this.setPayLine();
 		this.setButtonLine();
 		
-		
-		this.setResizable(false);
+		this.setLayout(null);
 		this.setVisible(true);
+		this.setResizable(false);
 	}
 	public void setImageLine() {
 		//상품을 구매하는사람에 따라 아이템이 다르니깐..? 라벨로 처리 했씁니당
-		
-		
+		this.imageIcon.setBounds(GamePayRoomEnum.GAME_ROOM_PAY_ITEM_LABEL.getRectangle());
+		this.imageIcon.setFont(GamePayRoomEnum.LABELFONT_DEFAULT.getFont());
+		this.add(imageIcon);	
 	}
 	public void setPayLine() {
+		this.q.setBounds(GamePayRoomEnum.GAME_ROOM_AMOUNT_PANEL.getRectangle());
+		this.q.setLayout(null);
+		this.q.setBackground(Color.blue);
+		
+		JTextField userAmountText = new JTextField();
+		userAmountText.setBounds(GamePayRoomEnum.GAME_ROOM_USER_AMOUNT_PANEL.getRectangle());
+		userAmountText.setFont(GamePayRoomEnum.LABELFONT_DEFAULT.getFont());
+		userAmountText.setBorder(GamePayRoomEnum.LABEL_DEFAULT.getEmptyBorder());
+		userAmountText.setBackground(Color.red);
+				
+
+		this.basicMoneyLabel.setBounds(GamePayRoomEnum.GAME_ROOM_PAY_BASICMONEY_LABEL.getRectangle());
+		this.basicMoneyLabel.setFont(GamePayRoomEnum.LABELFONT_DEFAULT.getFont());
+		
+		this.divLabel.setBounds(GamePayRoomEnum.GAME_ROOM_PAY_DIV_LABEL.getRectangle());
+		this.divLabel.setFont(GamePayRoomEnum.LABELFONT_DEFAULT.getFont());
+		
+		
+		this.equalsLabel.setBounds(GamePayRoomEnum.GAME_ROOM_PAY_EQUAL_LABEL.getRectangle());
+		this.equalsLabel.setFont(GamePayRoomEnum.LABELFONT_DEFAULT.getFont());
+		
+		this.totalAmountLabel.setBounds(GamePayRoomEnum.GAME_ROOM_PAY_TOTALMONEY_LABEL.getRectangle());
+		this.totalAmountLabel.setFont(GamePayRoomEnum.LABELFONT_DEFAULT.getFont());
+		
+
+		this.add(totalAmountLabel);
+		this.add(equalsLabel);
+		this.add(basicMoneyLabel);
+		this.add(divLabel);
+		//q.setOpaque(false);
+		
+		this.q.add(userAmountText);
+		this.add(q);
+		
+		System.out.println(GamePayRoomEnum.GAME_ROOM_PAY_ITEM_LABEL.getRectangle());
 
 	}
-	public void setButtonLine() {
+	public void setButtonLine() throws IOException {
+		this.payButton.setIconTextGap(this.payButton.getIconTextGap() - 15 );
+		this.payButton.setIcon(
+				new ImageIcon(ImageIO.read(
+						new File("resources/store/pay.png")).getScaledInstance(
+								GamePayRoomEnum.GAME_ROOM_PAY_BUTTON.getRectangle().width,
+								GamePayRoomEnum.GAME_ROOM_PAY_BUTTON.getRectangle().height,
+	    						Image.SCALE_AREA_AVERAGING))
+	    		);
+						
+		this.payButton.setBounds(GamePayRoomEnum.GAME_ROOM_PAY_BUTTON.getRectangle());
 		
+		
+		this.cancelButton.setIconTextGap(this.cancelButton.getIconTextGap() - 15 );
+		this.cancelButton.setIcon(
+				new ImageIcon(ImageIO.read(
+						new File("resources/myData/reset.kor.png")).getScaledInstance(
+								GamePayRoomEnum.GAME_ROOM_PAY_CANCEL_BUTTON.getRectangle().width,
+								GamePayRoomEnum.GAME_ROOM_PAY_CANCEL_BUTTON.getRectangle().height,
+	    						Image.SCALE_AREA_AVERAGING))
+	    		);
+						
+		this.cancelButton.setBounds(GamePayRoomEnum.GAME_ROOM_PAY_CANCEL_BUTTON.getRectangle());
+		
+	
+		this.add(payButton);
+		this.add(cancelButton);
 	}
 	
-	public static void main(String[] args) {
-		try {
-			new GamePayRoomFrame();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public static void main(String[] args) throws IOException {
+		new GamePayRoomFrame();
 
 	}
 
